b7ba99fa590b860c8532afba9e25bf24a6cd081d
Jarrad Law <jarrad.law@gmail.com>
2018-02-28 15:26:37 +1030

    Removed some junk from the code

--- app.js
+++ app.js
@@ -1,112 +1,98 @@
-// Load up the discord.js library
 const Discord = require("discord.js");
 
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
 client.on("ready", () => {
-    // This event will run if the bot starts, and logs in, successfully.
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    // Example of changing the bot's playing game to something useful. `client.user` is what the
-    // docs refer to as the "ClientUser".
     client.user.setActivity(`Pokemon Go`);
 });
 
 client.on("message", async message => {
     // This event will run on every single message received, from any channel or DM.
-
-    // It's good practice to ignore other bots. This also makes your bot ignore itself
-    // and not get into a spam loop (we call that "botception").
+    // Ignores messages from all bots
     if (message.author.bot) return;
 
-    // Also good practice to ignore any message that does not start with our prefix, 
-    // which is set in the configuration file.
+    // Ignores messages without prefix
     if (message.content.indexOf(config.prefix) !== 0) return;
 
-    // Here we separate our "command" name, and our "arguments" for the command. 
-    // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-    // command = say
-    // args = ["Is", "this", "the", "real", "life?"]
+    // Ignores messages not from "Admin" role
+    if (!message.member.roles.some(r => ["Admin"].includes(r.name))) return;
+
+    // Splits command into array
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
+    if (command === "exwelcome") {
+        message.delete().catch(O_o => { });
+        message.channel.send('Welcome to your EX raid family! Congratulations on getting a pass. So that we give everybody the best chance of success, please use these channels to co-ordinate between yourselves. Keep an eye out for each other at the raid. It is everybody\'s responsibility to make sure all EX raid pass carriers get a chance to catch this legendary Pokémon.Happy raiding!');
+        message.channel.send('------\nPlease react to this message with the number emoji of MYSTIC accounts you will be raiding with');
+        message.channel.send('------\nPlease react to this message with the number emoji of VALOR accounts you will be raiding with');
+        message.channel.send('------\nPlease react to this message with the number emoji of INSTINCT accounts you will be raiding with');
+        message.channel.send('------\n\n*Example: If I am responding for myself (Valor),  my wife (Valor) and my son (Mystic), I would react with a :two: to the Valor message and a :one: to the Mystic message.*');
+    } else if (command === "addexraid") {
+        message.delete().catch(O_o => { });
+        let server = message.guild;
 
-    if (!message.member.roles.some(r => ["Admin"].includes(r.name))) {
-        return;
-    } else {
-        if (command === "exwelcome") {
-            message.delete().catch(O_o => { });
-            message.channel.send('Welcome to your EX raid family! Congratulations on getting a pass. So that we give everybody the best chance of success, please use these channels to co-ordinate between yourselves. Keep an eye out for each other at the raid. It is everybody\'s responsibility to make sure all EX raid pass carriers get a chance to catch this legendary Pokémon.Happy raiding!');
-            message.channel.send('------\nPlease react to this message with the number emoji of MYSTIC accounts you will be raiding with');
-            message.channel.send('------\nPlease react to this message with the number emoji of VALOR accounts you will be raiding with');
-            message.channel.send('------\nPlease react to this message with the number emoji of INSTINCT accounts you will be raiding with');
-            message.channel.send('------\n\n*Example: If I am responding for myself (Valor),  my wife (Valor) and my son (Mystic), I would react with a :two: to the Valor message and a :one: to the Mystic message.*');
-        } else if (command === "addexraid") {
-            message.delete().catch(O_o => { });
-            let server = message.guild;
+        let raidName = args.shift() || 'exraid' + Math.floor(Math.random() * 10000);
+        let raidDesc = args.join(' ') || raidName;
 
-            let raidName = args.shift() || 'exraid' + Math.floor(Math.random() * 10000);
-            let raidDesc = args.join(' ') || raidName;
+        server.createChannel(raidName, "text")
+            .then(channel => {
+                channel.setParent(message.channel.parent);
 
+                channel.send('Welcome to your EX raid family! Congratulations on getting a pass. So that we give everybody the best chance of success, please use these channels to co-ordinate between yourselves. Keep an eye out for each other at the raid. It is everybody\'s responsibility to make sure all EX raid pass carriers get a chance to catch this legendary Pokémon.Happy raiding!');
+                channel.send('------\nPlease react to this message with the number emoji of MYSTIC accounts you will be raiding with');
+                channel.send('------\nPlease react to this message with the number emoji of VALOR accounts you will be raiding with');
+                channel.send('------\nPlease react to this message with the number emoji of INSTINCT accounts you will be raiding with');
+                channel.send('------\n\n*Example: If I am responding for myself (Valor),  my wife (Valor) and my son (Mystic), I would react with a :two: to the Valor message and a :one: to the Mystic message.*');
 
-            server.createChannel(raidName, "text")
-                .then(channel => {
-                    channel.setParent(message.channel.parent);
+                let exRole = message.guild.roles.find("name", "ExRaids");
+                channel.overwritePermissions(exRole, { READ_MESSAGES: true, SEND_MESSAGES: true });
+                let erroneRole = message.guild.roles.find("name", "@everyone");
+                channel.overwritePermissions(erroneRole, { READ_MESSAGES: false });
 
-                    channel.send('Welcome to your EX raid family! Congratulations on getting a pass. So that we give everybody the best chance of success, please use these channels to co-ordinate between yourselves. Keep an eye out for each other at the raid. It is everybody\'s responsibility to make sure all EX raid pass carriers get a chance to catch this legendary Pokémon.Happy raiding!');
-                    channel.send('------\nPlease react to this message with the number emoji of MYSTIC accounts you will be raiding with');
-                    channel.send('------\nPlease react to this message with the number emoji of VALOR accounts you will be raiding with');
-                    channel.send('------\nPlease react to this message with the number emoji of INSTINCT accounts you will be raiding with');
-                    channel.send('------\n\n*Example: If I am responding for myself (Valor),  my wife (Valor) and my son (Mystic), I would react with a :two: to the Valor message and a :one: to the Mystic message.*');
+                server.createRole({ name: raidName })
+                    .then(role => {
+                        channel.overwritePermissions(role, { READ_MESSAGES: true, SEND_MESSAGES: true, READ_MESSAGE_HISTORY: true });
+                    }).catch(console.error);
 
-                    let exRole = message.guild.roles.find("name", "ExRaids");
-                    channel.overwritePermissions(exRole, { READ_MESSAGES: true, SEND_MESSAGES: true });
-                    let erroneRole = message.guild.roles.find("name", "@everyone");
-                    channel.overwritePermissions(erroneRole, { READ_MESSAGES: false });
+                channel.setTopic(raidDesc).catch(console.error);
+            });
+    } else if (command === "deleteexraid") {
 
-                    server.createRole({ name: raidName })
-                        .then(role => {
-                            channel.overwritePermissions(role, { READ_MESSAGES: true, SEND_MESSAGES: true, READ_MESSAGE_HISTORY: true });
-                        }).catch(console.error);
+        message.delete().catch(O_o => { });
+        let role = message.guild.roles.find("name", message.channel.name);
+        if (role) role.delete();
+        message.channel.delete();
 
-                    var original = "Mon 25-Jul-2011";
-                    var result = original.substr(original.indexOf(" ") + 1);
+    } else if (command === "reply") {
 
-                    channel.setTopic(raidDesc).catch(console.error);
-                });
-        } else if (command === "deleteexraid") {
-            message.delete().catch(O_o => { });
-            let role = message.guild.roles.find("name", message.channel.name);
-            if (role) role.delete();
-            message.channel.delete();
-        } else if (command === "reply") {
-            message.delete().catch(O_o => { });
-            message.channel.send(':blush:');
-        } else if (command === "listexraids") {
-            let server = message.guild;
-            let msg = 'React to add yourself to the exraid channels\n\n';
-            let topic = '';
-            for (let i = 0; i < args.length; i += 2) {
-                topic = client.channels.find("name", args[i]);
-                msg += args[i + 1] + " : `" + topic.topic + "`\n\n";
-            }
-            message.channel.send(msg)
-                .then(message => {
-                    for (let i = 1; i < args.length; i += 2) {
-                        message.react(args[i]);
-                    }
-                }).catch(error => console.log(error));;
+        message.delete().catch(O_o => { });
+        message.channel.send(':blush:');
+
+    } else if (command === "listexraids") {
+
+        let server = message.guild;
+        let msg = 'React to add yourself to the exraid channels\n\n';
+        let topic = '';
+        for (let i = 0; i < args.length; i += 2) {
+            topic = client.channels.find("name", args[i]);
+            msg += args[i + 1] + " : `" + topic.topic + "`\n\n";
         }
+        message.channel.send(msg)
+            .then(message => {
+                for (let i = 1; i < args.length; i += 2) {
+                    message.react(args[i]);
+                }
+            }).catch(error => console.log(error));;
     }
 });
 
+// Such ugly code, but it works
 client.on('messageReactionAdd', (reaction, user) => {
     if (user.id !== '417089763967893504') {
         let str = reaction.message.content.split("\n");
